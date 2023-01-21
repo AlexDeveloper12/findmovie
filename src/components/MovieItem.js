2f975334fbd6ac65e97d82a68a6d00be1e0ff41c
AlexDeveloper12 <angrycoder95@gmail.com>
2023-01-21 11:45:55 +0000

    Adding movies.json to public folder

---
+++ src/components/MovieItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MovieItem(){
+    return(
+        <div>
+            Movie Item here
+        </div>
+    )
+
+}
+
+export default MovieItem;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,14 @@
 import React, {useState} from "react";
+import "../styles/Search.css";
 
-function Search(){
-
+function Search({searchValue}){
+    return(
+        <div className="search-movie-container">
+            <input type="text" className="search-movie"/>
+            <button type="button">Search</button>
+        </div>
+        
+    )
 }
 
 export default Search;
